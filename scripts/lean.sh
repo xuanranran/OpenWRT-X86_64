a43f5bdc8c468dc63e8d3bf72d6948acd7b62355
xuanranran <juanshengyuan@gmail.com>
2023-09-16 07:26:23 +0800

    Update lean.sh

--- scripts/lean.sh
+++ scripts/lean.sh
@@ -36,7 +36,7 @@ rm -rf openwrt-package/luci-app-verysync
 
 # Add luci-app-ssr-plus
 rm -rf package/community/helloworld
-git clone --depth=1 https://github.com/kenzok8/small.git package/community/helloworld
+git clone --depth=1 https://github.com/kenzok8/small.git
 # git clone --depth=1 https://github.com/fw876/helloworld.git package/community/helloworld
 
 # Add luci-app-passwall
