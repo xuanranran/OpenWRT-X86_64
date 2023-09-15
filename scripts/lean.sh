bf25f3278f5170aadc5995cfd2633072698c5cf9
xuanranran <juanshengyuan@gmail.com>
2023-09-15 21:23:39 +0800

    Update lean.sh

--- scripts/lean.sh
+++ scripts/lean.sh
@@ -38,6 +38,8 @@ rm -rf openwrt-package/luci-app-verysync
 git clone --depth=1 https://github.com/fw876/helloworld
 
 # Add luci-app-passwall
+git clone https://github.com/xiaorouji/openwrt-passwall
+git clone --depth=1 https://github.com/xiaorouji/openwrt-passwall2
 svn export https://github.com/xiaorouji/openwrt-passwall/trunk/luci-app-passwall package/luci-app-passwall
 svn export https://github.com/xiaorouji/openwrt-passwall2/trunk/luci-app-passwall2 package/luci-app-passwall2
 
