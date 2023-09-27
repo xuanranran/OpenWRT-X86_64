89b463f745be6e2bfb167eb0d5f199777603192d
xuanranran <juanshengyuan@gmail.com>
2023-09-27 20:57:24 +0800

    Update lean.sh

--- scripts/lean.sh
+++ scripts/lean.sh
@@ -113,7 +113,7 @@ popd
 
 # 在线用户
 svn export https://github.com/haiibo/packages/trunk/luci-app-onliner package/luci-app-onliner
-sed -i '$i uci set nlbwmon.@nlbwmon[0].refresh_interval=2s' package/lean/default-settings/files/zzz-default-settings
+sed -i '$i uci set nlbwmon.@nlbwmon[0].refresh_interval=1s' package/lean/default-settings/files/zzz-default-settings
 sed -i '$i uci commit nlbwmon' package/lean/default-settings/files/zzz-default-settings
 chmod 755 package/luci-app-onliner/root/usr/share/onliner/setnlbw.sh
 
