68bf59f17031976f2fdb221c48ac9d21b872aa1c
xuanranran <juanshengyuan@gmail.com>
2023-11-06 19:22:59 +0800

    upd10

--- scripts/lean_test.sh
+++ scripts/lean_test.sh
@@ -129,7 +129,7 @@ chmod 755 package/luci-app-onliner/root/usr/share/onliner/setnlbw.sh
 # Mod zzz-default-settings
 pushd package/lean/default-settings/files
 sed -i '/http/d' zzz-default-settings
-sed -i '/23.05/d' zzz-default-settings
+# sed -i '/18.06/d' zzz-default-settings
 export orig_version=$(cat "zzz-default-settings" | grep DISTRIB_REVISION= | awk -F "'" '{print $2}')
 export date_version=$(date -d "$(rdate -n -4 -p ntp.aliyun.com)" +'%Y-%m-%d')
 sed -i "s/${orig_version}/${orig_version} (${date_version})/g" zzz-default-settings
