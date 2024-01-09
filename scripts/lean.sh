8b81e69b5e28caf715ea11f04de6e196d2aa51fb
xuanranran <juanshengyuan@gmail.com>
2024-01-09 23:20:06 +0800

    Update lean.sh

--- scripts/lean.sh
+++ scripts/lean.sh
@@ -131,6 +131,7 @@ sed -i 's/os.date()/os.date("%F %T %a")/g' package/lean/autocore/files/*/index.h
 rm -rf target/linux/x86/base-files/etc/board.d/02_network
 cp -f $GITHUB_WORKSPACE/02_network target/linux/x86/base-files/etc/board.d/02_network
 rm -rf package/base-files/files/etc/banner
-wget -P package/base-files/files/etc https://raw.githubusercontent.com/DHDAXCW/lede-rockchip/stable/package/base-files/files/etc/banner
+cp -f $GITHUB_WORKSPACE/data/banner package/base-files/files/etc/banner
+# wget -P package/base-files/files/etc https://raw.githubusercontent.com/DHDAXCW/lede-rockchip/stable/package/base-files/files/etc/banner
 # sed -i 's/6.1/5.10/g' target/linux/x86/Makefile
 # cp -r ../target/linux/generic/pending-6.1/ ./target/linux/generic/
