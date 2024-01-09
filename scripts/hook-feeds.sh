accc451069a2b11969a4c38172edc367320448ce
xuanranran <juanshengyuan@gmail.com>
2024-01-09 15:07:26 +0800

    Update hook-feeds.sh

--- scripts/hook-feeds.sh
+++ scripts/hook-feeds.sh
@@ -24,8 +24,6 @@ git clone --depth=1 https://github.com/immortalwrt/openwrt-tmate
 
 echo >> feeds.conf.default
 echo 'src-git istore https://github.com/linkease/istore;main' >> feeds.conf.default
-./scripts/feeds update istore
-./scripts/feeds install -d y -p istore luci-app-store
 
 # Set to local feeds
 pushd customfeeds/packages
