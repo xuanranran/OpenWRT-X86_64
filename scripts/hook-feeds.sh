073da9e40c4c9a16583d18955f7704a356436f23
xuanranran <juanshengyuan@gmail.com>
2024-01-11 21:48:27 +0800

    Update hook-feeds.sh

--- scripts/hook-feeds.sh
+++ scripts/hook-feeds.sh
@@ -2,14 +2,6 @@
 # Svn checkout packages from immortalwrt's repository
 pushd customfeeds
 
-# Add luci-proto-modemmanager
-# svn co https://github.com/immortalwrt/luci/trunk/protocols/luci-proto-modemmanager luci/protocols/luci-proto-modemmanager
-
-# Add luci-app-gowebdav
-# svn co https://github.com/immortalwrt/luci/trunk/applications/luci-app-gowebdav luci/applications/luci-app-gowebdav
-# rm -rf packages/net/gowebdav
-# svn co https://github.com/immortalwrt/packages/trunk/net/gowebdav packages/net/gowebdav
-
 # Add tmate
 git clone --depth=1 https://github.com/immortalwrt/openwrt-tmate
 
