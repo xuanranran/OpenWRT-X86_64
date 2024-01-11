cbadbd39414d72158304650250c800042739f364
xuanranran <juanshengyuan@gmail.com>
2024-01-11 21:48:30 +0800

    Update lean.sh

--- scripts/lean.sh
+++ scripts/lean.sh
@@ -12,98 +12,41 @@
 rm -rf package/wwan/driver/quectel_MHI
 cp -r $GITHUB_WORKSPACE/data/quectel_MHI package/wwan/driver/quectel_MHI
 
-# alist
-git clone https://github.com/sbwml/luci-app-alist package/alist
 # rm -rf feeds/packages/lang/golang
 # git clone https://github.com/sbwml/packages_lang_golang -b 21.x feeds/packages/lang/golang
 
 mkdir package/community
 pushd package/community
-
-# netdata
 rm -rf ../../customfeeds/luci/applications/luci-app-netdata
-git clone --depth=1 https://github.com/sirpdboy/luci-app-netdata ../../customfeeds/luci/applications/luci-app-netdata
-
-# Add Lienol's Packages
-git clone --depth=1 https://github.com/Lienol/openwrt-package
 rm -rf ../../customfeeds/luci/applications/luci-app-kodexplorer
-rm -rf openwrt-package/verysync
-rm -rf openwrt-package/luci-app-verysync
-
-# Add luci-app-ssr-plus
 rm -rf package/community/helloworld
-git clone --depth=1 https://github.com/fw876/helloworld package/helloworld
-
-# add luci-app-daed
-# git clone https://github.com/sbwml/luci-app-daed-next
-
-# Add luci-app-passwall
-git clone --depth=1 https://github.com/xiaorouji/openwrt-passwall-packages
-git clone --depth=1 https://github.com/xiaorouji/openwrt-passwall
-git clone --depth=1 https://github.com/xiaorouji/openwrt-passwall2
-
-# Add luci-app-unblockneteasemusic
 rm -rf ../../customfeeds/luci/applications/luci-app-unblockmusic
-git clone --depth=1 https://github.com/UnblockNeteaseMusic/luci-app-unblockneteasemusic.git
-
-# Add luci-app-vssr <M>
-git clone --depth=1 https://github.com/xuanranran/luci-app-vssr
-git clone --depth=1 https://github.com/jerrykuku/lua-maxminddb.git
-
-# Add luci-proto-minieap
-git clone --depth=1 https://github.com/ysc3839/luci-proto-minieap
-
-# Add luci-aliyundrive-webdav
 rm -rf ../../customfeeds/luci/applications/luci-app-aliyundrive-webdav 
 rm -rf ../../customfeeds/packages/multimedia/aliyundrive-webdav
-
-# Add ddnsto & linkease
-# git clone --depth=1 https://github.com/linkease/nas-packages-luci package/nas-packages-luci
-git clone --depth=1 https://github.com/linkease/nas-packages package/nas-packages
-
-# Add OpenClash
-git clone --depth=1 -b dev https://github.com/vernesong/OpenClash package/OpenClash
-
-# Add luci-app-poweroff
-git clone --depth=1 https://github.com/esirplayground/luci-app-poweroff
-
-# Add luci-theme
-git clone --depth=1 -b 18.06 https://github.com/jerrykuku/luci-theme-argon
-git clone --depth=1 https://github.com/jerrykuku/luci-app-argon-config
 rm -rf ../../customfeeds/luci/themes/luci-theme-argon
 rm -rf ../../customfeeds/luci/themes/luci-theme-argon-mod
+popd
+
+# Add openwrt-packages
+git clone --depth=1 https://github.com/xuanranran/openwrt-packages package/openwrt-packages
+pushd package/openwrt-packages
+
 rm -rf ./luci-theme-argon/htdocs/luci-static/argon/img/bg1.jpg
 cp -f $GITHUB_WORKSPACE/data/bg1.jpg luci-theme-argon/htdocs/luci-static/argon/img/bg1.jpg
-git clone https://github.com/DHDAXCW/theme
 
 # Add subconverter
 git clone --depth=1 https://github.com/tindy2013/openwrt-subconverter
 
-# Add OpenAppFilter
-git clone --depth=1 https://github.com/destan19/OpenAppFilter
-popd
-
-# Add haiibo/openwrt-packages
-git clone https://github.com/haiibo/openwrt-packages package/openwrt-packages
-pushd package/openwrt-packages
-
-# Add luci-app-iptvhelper
-git clone --depth=1 https://github.com/riverscn/openwrt-iptvhelper
-
-# Add gost
-git clone --depth=1 https://github.com/xuanranran/gost
-popd
-
 # 在线用户
-# svn export https://github.com/haiibo/packages/trunk/luci-app-onliner package/luci-app-onliner
 sed -i '$i uci set nlbwmon.@nlbwmon[0].refresh_interval=1s' package/lean/default-settings/files/zzz-default-settings
 sed -i '$i uci commit nlbwmon' package/lean/default-settings/files/zzz-default-settings
-chmod 755 package/openwrt-packages/luci-app-onliner/root/usr/share/onliner/setnlbw.sh
+chmod 755 luci-app-onliner/root/usr/share/onliner/setnlbw.sh
+popd
 
 # Mod zzz-default-settings
 pushd package/lean/default-settings/files
 sed -i '/http/d' zzz-default-settings
-sed -i '/18.06/d' zzz-default-settings
+# sed -i '/18.06/d' zzz-default-settings
 export orig_version=$(cat "zzz-default-settings" | grep DISTRIB_REVISION= | awk -F "'" '{print $2}')
 export date_version=$(date -d "$(rdate -n -4 -p ntp.aliyun.com)" +'%Y-%m-%d')
 sed -i "s/${orig_version}/${orig_version} (${date_version})/g" zzz-default-settings
