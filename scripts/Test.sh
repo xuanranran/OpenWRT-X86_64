3aa74228387e9a9a81b0a15fe5490e14430459ee
LovinYarn <juanshengyuan@gmail.com>
2024-01-13 14:18:05 +0800

    Update Test.sh

--- scripts/Test.sh
+++ scripts/Test.sh
@@ -9,8 +9,8 @@
 #=================================================
 # Clone community packages to package/community
 
-rm -rf package/wwan/driver/quectel_MHI
-cp -r $GITHUB_WORKSPACE/data/quectel_MHI package/wwan/driver/quectel_MHI
+#rm -rf package/wwan/driver/quectel_MHI
+#cp -r $GITHUB_WORKSPACE/data/quectel_MHI package/wwan/driver/quectel_MHI
 
 # rm -rf feeds/packages/lang/golang
 # git clone https://github.com/sbwml/packages_lang_golang -b 21.x feeds/packages/lang/golang
@@ -24,30 +24,29 @@ rm -rf ../../customfeeds/luci/applications/luci-app-aliyundrive-webdav
 rm -rf ../../customfeeds/packages/multimedia/aliyundrive-webdav
 rm -rf ../../customfeeds/luci/themes/luci-theme-argon
 rm -rf ../../customfeeds/luci/themes/luci-theme-argon-mod
-rm -rf ../../staging_dir
 
 # Add openwrt-packages
-git clone --depth=1 https://github.com/xuanranran/openwrt-packages openwrt-packages
-git clone --depth=1 https://github.com/xuanranran/rely
-rm -rf ./openwrt-packages/luci-theme-argon/htdocs/luci-static/argon/img/bg1.jpg
-rm -rf ./openwrt-packages/luci-theme-design/htdocs/luci-static/design/favicon.ico
-cp -f $GITHUB_WORKSPACE/data/bg1.jpg openwrt-packages/luci-theme-argon/htdocs/luci-static/argon/img/bg1.jpg
-cp -f $GITHUB_WORKSPACE/data/favicon.ico openwrt-packages/luci-theme-design/htdocs/luci-static/design/favicon.ico
+#git clone --depth=1 https://github.com/xuanranran/openwrt-packages openwrt-packages
+#git clone --depth=1 https://github.com/xuanranran/rely
+#rm -rf ./openwrt-packages/luci-theme-argon/htdocs/luci-static/argon/img/bg1.jpg
+#rm -rf ./openwrt-packages/luci-theme-design/htdocs/luci-static/design/favicon.ico
+#cp -f $GITHUB_WORKSPACE/data/bg1.jpg openwrt-packages/luci-theme-argon/htdocs/luci-static/argon/img/bg1.jpg
+#cp -f $GITHUB_WORKSPACE/data/favicon.ico openwrt-packages/luci-theme-design/htdocs/luci-static/design/favicon.ico
 
 # 在线用户
-chmod 755 openwrt-packages/luci-app-onliner/root/usr/share/onliner/setnlbw.sh
-popd
+#chmod 755 openwrt-packages/luci-app-onliner/root/usr/share/onliner/setnlbw.sh
+#popd
 
 # Mod zzz-default-settings
-pushd package/lean/default-settings/files
-sed -i '$i uci set nlbwmon.@nlbwmon[0].refresh_interval=1s' zzz-default-settings
-sed -i '$i uci commit nlbwmon' zzz-default-settings
-sed -i '/http/d' zzz-default-settings
+#pushd package/lean/default-settings/files
+#sed -i '$i uci set nlbwmon.@nlbwmon[0].refresh_interval=1s' zzz-default-settings
+#sed -i '$i uci commit nlbwmon' zzz-default-settings
+#sed -i '/http/d' zzz-default-settings
 # sed -i '/18.06/d' zzz-default-settings
-export orig_version=$(cat "zzz-default-settings" | grep DISTRIB_REVISION= | awk -F "'" '{print $2}')
-export date_version=$(date -d "$(rdate -n -4 -p ntp.aliyun.com)" +'%Y-%m-%d')
-sed -i "s/${orig_version}/${orig_version} (${date_version})/g" zzz-default-settings
-popd
+#export orig_version=$(cat "zzz-default-settings" | grep DISTRIB_REVISION= | awk -F "'" '{print $2}')
+#export date_version=$(date -d "$(rdate -n -4 -p ntp.aliyun.com)" +'%Y-%m-%d')
+#sed -i "s/${orig_version}/${orig_version} (${date_version})/g" zzz-default-settings
+#popd
 
 # Fix libssh
 # pushd feeds/packages/libs
