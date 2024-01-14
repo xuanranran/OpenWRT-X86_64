9b028477601be4228a0b3455ed3865c74f3355f9
LovinYarn <juanshengyuan@gmail.com>
2024-01-15 02:10:13 +0800

    Update hook-feeds.sh

--- scripts/hook-feeds.sh
+++ scripts/hook-feeds.sh
@@ -6,7 +6,7 @@ popd
 pushd customfeeds/luci
 export luci_feed="$(pwd)"
 popd
-# cp -r $GITHUB_WORKSPACE/data/pcre2 customfeeds/packages/libs/pcre2
+cp -r $GITHUB_WORKSPACE/data/pcre2 customfeeds/packages/libs/pcre2
 sed -i '/src-git packages/d' feeds.conf.default
 echo "src-link packages $packages_feed" >> feeds.conf.default
 sed -i '/src-git luci/d' feeds.conf.default
