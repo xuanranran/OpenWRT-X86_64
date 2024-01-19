1e941181f21299cb8544449201349950a2cbe2bc
xuanranran <juanshengyuan@gmail.com>
2024-01-19 21:09:05 +0800

    Update init-settings.sh

--- scripts/init-settings.sh
+++ scripts/init-settings.sh
@@ -8,7 +8,7 @@
 # Blog: https://mlapp.cn
 #=================================================
 # Set default theme to luci-theme-argon luci-app-design
-uci set luci.main.mediaurlbase='/luci-static/argon'
+uci set luci.main.mediaurlbase='/luci-static/design'
 
 # Disable opkg signature check
 # sed -i 's/option check_signature/# option check_signature/g' /etc/opkg.conf
