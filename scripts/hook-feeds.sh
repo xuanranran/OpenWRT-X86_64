045f45aa3563cd3e719a423261e4b4d105f10544
LovinYarn <juanshengyuan@gmail.com>
2024-01-30 03:05:32 +0800

    Update hook-feeds.sh

--- scripts/hook-feeds.sh
+++ scripts/hook-feeds.sh
@@ -7,8 +7,10 @@ pushd customfeeds/luci
 export luci_feed="$(pwd)"
 popd
 # rm -rf package/network/utils/uqmi
+rm -rf package/net/iperf3
 # cp -r $GITHUB_WORKSPACE/data/uqmi package/network/utils/uqmi
 cp -r $GITHUB_WORKSPACE/data/xdp-tools package/network/utils/xdp-tools
+cp -r $GITHUB_WORKSPACE/data/iperf3 package/net/iperf3
 # cp -r $GITHUB_WORKSPACE/data/pcre2 customfeeds/packages/libs/pcre2
 sed -i '/src-git packages/d' feeds.conf.default
 echo "src-link packages $packages_feed" >> feeds.conf.default
