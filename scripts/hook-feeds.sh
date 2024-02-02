7c9455ddca18b2c85df15260423ec1d922a3246c
xuanranran <juanshengyuan@gmail.com>
2024-02-02 19:32:26 +0800

    Update hook-feeds.sh

--- scripts/hook-feeds.sh
+++ scripts/hook-feeds.sh
@@ -6,11 +6,11 @@ popd
 pushd customfeeds/luci
 export luci_feed="$(pwd)"
 popd
-cp -r $GITHUB_WORKSPACE/data/xdp-tools package/network/utils/xdp-tools
 rm -rf config/Config-kernel.in
-cp -r $GITHUB_WORKSPACE/data/Config-kernel.in config/Config-kernel.in
 rm -rf package/kernel/linux/modules/netsupport.mk
+cp -r $GITHUB_WORKSPACE/data/Config-kernel.in config/Config-kernel.in
 cp -r $GITHUB_WORKSPACE/data/netsupport.mk package/kernel/linux/modules/netsupport.mk
+cp -r $GITHUB_WORKSPACE/data/xdp-tools package/network/utils/xdp-tools
 sed -i '/src-git packages/d' feeds.conf.default
 echo "src-link packages $packages_feed" >> feeds.conf.default
 sed -i '/src-git luci/d' feeds.conf.default
