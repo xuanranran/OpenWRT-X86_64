1576c1c1fcac0491117b8b366979116f360340f4
xuanranran <juanshengyuan@gmail.com>
2024-02-03 18:06:50 +0800

    *****

--- scripts/hook-feeds.sh
+++ scripts/hook-feeds.sh
@@ -15,7 +15,7 @@ sed -i '/src-git packages/d' feeds.conf.default
 echo "src-link packages $packages_feed" >> feeds.conf.default
 sed -i '/src-git luci/d' feeds.conf.default
 echo "src-link luci $luci_feed" >> feeds.conf.default
-echo "src-git daed https://github.com/xuanranran/openwrt-daed.git" >> feeds.conf.default
+# echo "src-git daed https://github.com/xuanranran/openwrt-daed.git" >> feeds.conf.default
 
 # Update feeds
 ./scripts/feeds update -a
