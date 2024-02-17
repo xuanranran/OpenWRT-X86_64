a92cd9fe606463b0b7f903dcd6b91b28eb81cbb3
xuanranran <juanshengyuan@gmail.com>
2024-02-17 19:31:32 +0800

    fix

--- scripts/hook-feeds.sh
+++ scripts/hook-feeds.sh
@@ -9,12 +9,15 @@ popd
 rm -rf config/Config-kernel.in
 rm -rf package/kernel/linux/modules/netsupport.mk
 rm -rf toolchain/gcc/*
+rm -rf package/libs/elfutils
 pushd toolchain/gcc/
 git clone --depth 1 https://github.com/immortalwrt/immortalwrt immortalwrt && mv -n immortalwrt/toolchain/gcc/* ./ ; rm -rf immortalwrt
 popd
+
 cp -r $GITHUB_WORKSPACE/data/config/Config-kernel.in config/Config-kernel.in
 cp -r $GITHUB_WORKSPACE/data/netsupport.mk package/kernel/linux/modules/netsupport.mk
 cp -r $GITHUB_WORKSPACE/data/xdp-tools package/network/utils/xdp-tools
+cp -r $GITHUB_WORKSPACE/data/package/libs/elfutils package/libs/elfutils
 sed -i '/src-git packages/d' feeds.conf.default
 echo "src-link packages $packages_feed" >> feeds.conf.default
 sed -i '/src-git luci/d' feeds.conf.default
