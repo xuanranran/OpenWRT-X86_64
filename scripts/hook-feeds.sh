61713cc6f8cfd91794ea0fdb74561c5c889fa6cc
xuanranran <juanshengyuan@gmail.com>
2024-02-17 17:32:24 +0800

    Update hook-feeds.sh

--- scripts/hook-feeds.sh
+++ scripts/hook-feeds.sh
@@ -8,11 +8,13 @@ export luci_feed="$(pwd)"
 popd
 rm -rf config/Config-kernel.in
 rm -rf package/kernel/linux/modules/netsupport.mk
-rm -rf toolchain/gcc/Config.in 
+rm -rf toolchain/gcc/*
+pushd toolchain/gcc/
+git clone --depth 1 https://github.com/immortalwrt/immortalwrt immortalwrt && mv -n immortalwrt/toolchain/gcc/* ./ ; rm -rf immortalwrt
+popd
 cp -r $GITHUB_WORKSPACE/data/config/Config-kernel.in config/Config-kernel.in
 cp -r $GITHUB_WORKSPACE/data/netsupport.mk package/kernel/linux/modules/netsupport.mk
 cp -r $GITHUB_WORKSPACE/data/xdp-tools package/network/utils/xdp-tools
-cp -r $GITHUB_WORKSPACE/data/toolchain/gcc/Config.in toolchain/gcc/Config.in 
 sed -i '/src-git packages/d' feeds.conf.default
 echo "src-link packages $packages_feed" >> feeds.conf.default
 sed -i '/src-git luci/d' feeds.conf.default
