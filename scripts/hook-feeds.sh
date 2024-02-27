2a5f010bb5c193739988442cd598ef3532057227
xuanranran <juanshengyuan@gmail.com>
2024-02-27 20:36:31 +0800

    ****

--- scripts/hook-feeds.sh
+++ scripts/hook-feeds.sh
@@ -8,6 +8,7 @@ export luci_feed="$(pwd)"
 popd
 rm -rf config/Config-kernel.in
 rm -rf package/kernel/linux/modules/netsupport.mk
+rm -rf package/kernel/linux/modules/video.mk
 rm -rf toolchain/gcc/*
 rm -rf package/libs/elfutils
 rm -rf package/network/utils/uqmi
@@ -33,9 +34,10 @@ popd
 rm -rf feeds/packages/lang/golang
 git clone https://github.com/sbwml/packages_lang_golang -b 22.x feeds/packages/lang/golang
 
+cp -r $GITHUB_WORKSPACE/data/package/kernel/linux/modules/video.mk package/kernel/linux/modules/video.mk
+cp -r $GITHUB_WORKSPACE/data/package/kernel/linux/modules/netsupport.mk package/kernel/linux/modules/netsupport.mk
 cp -r $GITHUB_WORKSPACE/data/package/network/utils/uqmi package/network/utils/uqmi
 cp -r $GITHUB_WORKSPACE/data/config/Config-kernel.in config/Config-kernel.in
-cp -r $GITHUB_WORKSPACE/data/netsupport.mk package/kernel/linux/modules/netsupport.mk
 cp -r $GITHUB_WORKSPACE/data/xdp-tools package/network/utils/xdp-tools
 cp -r $GITHUB_WORKSPACE/data/package/libs/elfutils package/libs/elfutils
 sed -i '/src-git packages/d' feeds.conf.default
