6f06c14c15c624ecab9e21c41eb6b88ce55a1543
LovinYarn <juanshengyuan@gmail.com>
2024-10-15 13:03:33 +0800

    Update hook-feeds.sh

--- scripts/hook-feeds.sh
+++ scripts/hook-feeds.sh
@@ -46,6 +46,7 @@ git clone https://github.com/sbwml/packages_lang_golang customfeeds/packages/lan
 pushd package/network/config
 git clone --depth 1 https://github.com/immortalwrt/immortalwrt immortalwrt && mv -n immortalwrt/package/network/config/{*firewall,firewall4} ./ ; rm -rf immortalwrt 
 popd
+sed -i 's/+firewall/+uci-firewall/g' customfeeds/luci/applications/luci-app-firewall/Makefile
 
 # cp -r $GITHUB_WORKSPACE/data/package/kernel/linux/modules/video.mk package/kernel/linux/modules/video.mk
 # cp -r $GITHUB_WORKSPACE/data/config/Config-kernel.in config/Config-kernel.in
