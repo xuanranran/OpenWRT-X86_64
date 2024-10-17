f6b35e2eb5d5918d74667c02bc9cfc578bd52f49
LovinYarn <juanshengyuan@gmail.com>
2024-10-17 13:44:13 +0800

    Update hook-feeds.sh

--- scripts/hook-feeds.sh
+++ scripts/hook-feeds.sh
@@ -6,9 +6,9 @@ popd
 pushd customfeeds/luci
 export luci_feed="$(pwd)"
 popd
-rm -rf config/Config-kernel.in
+# rm -rf config/Config-kernel.in
 rm -rf package/kernel/linux/modules/netsupport.mk
-rm -rf package/kernel/linux/modules/video.mk
+# rm -rf package/kernel/linux/modules/video.mk
 rm -rf toolchain/gcc/*
 rm -rf package/libs/elfutils
 rm -rf package/network/utils/uqmi
@@ -53,8 +53,8 @@ pushd customfeeds/packages/lang/rust/
 git clone --depth 1 https://github.com/immortalwrt/packages rust && mv -n rust/lang/rust/* ./ ; rm -rf rust
 popd
 
-cp -r $GITHUB_WORKSPACE/data/package/kernel/linux/modules/video.mk package/kernel/linux/modules/video.mk
-cp -r $GITHUB_WORKSPACE/data/config/Config-kernel.in config/Config-kernel.in
+# cp -r $GITHUB_WORKSPACE/data/package/kernel/linux/modules/video.mk package/kernel/linux/modules/video.mk
+# cp -r $GITHUB_WORKSPACE/data/config/Config-kernel.in config/Config-kernel.in
 cp -r $GITHUB_WORKSPACE/data/package/kernel/linux/modules/netsupport.mk package/kernel/linux/modules/netsupport.mk
 cp -r $GITHUB_WORKSPACE/data/package/network/utils/uqmi package/network/utils/uqmi
 cp -r $GITHUB_WORKSPACE/data/xdp-tools package/network/utils/xdp-tools
