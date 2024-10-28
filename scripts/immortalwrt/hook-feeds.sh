d906e79ea84c806e84d82cf2a6e378dc77efbf73
LovinYarn <juanshengyuan@gmail.com>
2024-10-28 22:47:47 +0800

    1

--- scripts/immortalwrt/hook-feeds.sh
+++ scripts/immortalwrt/hook-feeds.sh
@@ -1,11 +1,11 @@
 #!/bin/bash
 # Set to local feeds
-# pushd customfeeds/lovepackages
-# export lovepackages_feed="$(pwd)"
-# popd
-# pushd customfeeds/loverely
-# export loverely_feed="$(pwd)"
-# popd
+pushd customfeeds/lovepackages
+export lovepackages_feed="$(pwd)"
+popd
+pushd customfeeds/loverely
+export loverely_feed="$(pwd)"
+popd
 
 pushd customfeeds/packages
 export packages_feed="$(pwd)"
@@ -21,10 +21,10 @@ pushd customfeeds/telephony
 export telephony_feed="$(pwd)"
 popd
 
-# sed -i '/src-git lovepackages/d' feeds.conf.default
-# echo "src-link lovepackages $lovepackages_feed" >> feeds.conf.default
-# sed -i '/src-git loverely/d' feeds.conf.default
-# echo "src-link loverely $loverely_feed" >> feeds.conf.default
+sed -i '/src-git lovepackages/d' feeds.conf.default
+echo "src-link lovepackages $lovepackages_feed" >> feeds.conf.default
+sed -i '/src-git loverely/d' feeds.conf.default
+echo "src-link loverely $loverely_feed" >> feeds.conf.default
 
 sed -i '/src-git packages/d' feeds.conf.default
 echo "src-link packages $packages_feed" >> feeds.conf.default
@@ -37,5 +37,5 @@ sed -i '/src-git telephony/d' feeds.conf.default
 echo "src-link telephony $telephony_feed" >> feeds.conf.default
 
 # Update feeds
-# ./scripts/feeds clean -a
+./scripts/feeds clean -a
 ./scripts/feeds update -a
