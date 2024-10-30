787e3a5740418dd9cd4198a469b5b8db68fc81b5
LovinYarn <juanshengyuan@gmail.com>
2024-10-30 10:13:23 +0800

    Update immortalwrt-init-settings.sh

--- scripts/immortalwrt-init-settings.sh
+++ scripts/immortalwrt-init-settings.sh
@@ -10,7 +10,7 @@
 # Set default theme to luci-theme-argon
 # theme
 uci set luci.main.mediaurlbase='/luci-static/argon'
-uci set luci.main.pollinterval='2'
+uci set luci.main.pollinterval='1'
 uci commit luci
 
 # timezone
