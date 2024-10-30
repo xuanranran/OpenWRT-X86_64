18a053a2fc46d0481df1dfa96b95d44a316db7b3
LovinYarn <juanshengyuan@gmail.com>
2024-10-31 01:42:09 +0800

    1

--- scripts/immortalwrt/hook-package.sh
+++ scripts/immortalwrt/hook-package.sh
@@ -162,8 +162,12 @@ curl -s https://github.com/openwrt/packages/commit/c0683d3f012096fc7b2fbe8b8dc81
 popd
 
 # perl
-sed -i "/Target perl/i\TARGET_CFLAGS_PERL += -Wno-implicit-function-declaration -Wno-int-conversion\n" customfeeds/packages/lang/perl/Makefile
-sed -i '/HOST_BUILD_PARALLEL/aPKG_BUILD_FLAGS:=no-mold' customfeeds/packages/lang/perl/Makefile
+# rm -rf customfeeds/packages/lang/perl
+# pushd customfeeds/packages/lang/
+# git clone --depth 1 https://github.com/coolsnowwolf/packages coolsnowwolf_perl && mv -n coolsnowwolf_perl/lang/perl ./ ; rm -rf coolsnowwolf_perl
+# popd
+# sed -i "/Target perl/i\TARGET_CFLAGS_PERL += -Wno-implicit-function-declaration -Wno-int-conversion\n" customfeeds/packages/lang/perl/Makefile
+# sed -i '/HOST_BUILD_PARALLEL/aPKG_BUILD_FLAGS:=no-mold' customfeeds/packages/lang/perl/Makefile
 
 # telephony
 pushd customfeeds/telephony
