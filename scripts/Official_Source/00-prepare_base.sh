796694b16fb883c57a9245910d99703574cdae80
LovinYarn <juanshengyuan@gmail.com>
2024-11-01 15:38:52 +0800

    test 24.10

--- scripts/Official_Source/00-prepare_base.sh
+++ scripts/Official_Source/00-prepare_base.sh
@@ -154,6 +154,7 @@ curl -s https://raw.githubusercontent.com/xuanranran/r4s_build_script/refs/heads
 curl -s https://raw.githubusercontent.com/xuanranran/r4s_build_script/refs/heads/master/openwrt/patch/firewall4/openwrt-24.10/0004-luci-add-firewall-add-custom-nft-rule-support.patch | patch -p1
 curl -s https://raw.githubusercontent.com/xuanranran/r4s_build_script/refs/heads/master/openwrt/patch/firewall4/openwrt-24.10/0005-luci-app-firewall-add-natflow-offload-support.patch | patch -p1
 curl -s https://raw.githubusercontent.com/xuanranran/r4s_build_script/refs/heads/master/openwrt/patch/firewall4/openwrt-24.10/0006-luci-app-firewall-enable-hardware-offload-only-on-de.patch | patch -p1
+popd
 
 # openssl - quictls
 pushd package/libs/openssl/patches
