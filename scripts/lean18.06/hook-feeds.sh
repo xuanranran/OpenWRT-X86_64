262f494f22f040832ab8463256069873fd494824
LovinYarn <juanshengyuan@gmail.com>
2024-11-03 23:14:31 +0800

    Update hook-feeds.sh

--- scripts/lean18.06/hook-feeds.sh
+++ scripts/lean18.06/hook-feeds.sh
@@ -43,6 +43,6 @@ sed -i '/src-git packages/d' feeds.conf.default
 echo "src-link packages $packages_feed" >> feeds.conf.default
 sed -i '/src-git luci/d' feeds.conf.default
 echo "src-link luci $luci_feed" >> feeds.conf.default
+
 # Update feeds
-./scripts/feeds clean -a
 ./scripts/feeds update -a
