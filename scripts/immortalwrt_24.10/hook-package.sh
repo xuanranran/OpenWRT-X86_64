aee59b3cecec0e52bfdf520fb44415e685cd6c9f
LovinYarn <juanshengyuan@gmail.com>
2024-11-11 13:09:36 +0800

    chore: refine

--- scripts/immortalwrt_24.10/hook-package.sh
+++ scripts/immortalwrt_24.10/hook-package.sh
@@ -54,12 +54,12 @@ curl -s https://raw.githubusercontent.com/xuanranran/r4s_build_script/refs/heads
 popd
 
 # Realtek driver - R8168 & R8125 & R8126 & R8152 & R8101
-rm -rf package/kernel/r8168 package/kernel/r8152 package/kernel/r8101 package/kernel/r8125 package/kernel/r8126
-git clone https://github.com/sbwml/package_kernel_r8168 package/kernel/r8168
-git clone https://github.com/sbwml/package_kernel_r8152 package/kernel/r8152
-git clone https://github.com/sbwml/package_kernel_r8101 package/kernel/r8101
-git clone https://github.com/sbwml/package_kernel_r8125 package/kernel/r8125
-git clone https://github.com/sbwml/package_kernel_r8126 package/kernel/r8126
+# rm -rf package/kernel/r8168 package/kernel/r8152 package/kernel/r8101 package/kernel/r8125 package/kernel/r8126
+# git clone https://github.com/sbwml/package_kernel_r8168 package/kernel/r8168
+# git clone https://github.com/sbwml/package_kernel_r8152 package/kernel/r8152
+# git clone https://github.com/sbwml/package_kernel_r8101 package/kernel/r8101
+# git clone https://github.com/sbwml/package_kernel_r8125 package/kernel/r8125
+# git clone https://github.com/sbwml/package_kernel_r8126 package/kernel/r8126
 
 # procps-ng - top
 sed -i 's/enable-skill/enable-skill --disable-modern-top/g' customfeeds/packages/utils/procps-ng/Makefile
