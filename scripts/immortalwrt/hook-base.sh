abf33f6f8eb41c2c3e357d2a224293baf530df77
LovinYarn <juanshengyuan@gmail.com>
2024-11-14 21:43:15 +0800

    Update hook-base.sh

--- scripts/immortalwrt/hook-base.sh
+++ scripts/immortalwrt/hook-base.sh
@@ -18,7 +18,7 @@ git clone https://github.com/sbwml/package_utils_util-linux -b openwrt-24.10 pac
 # git clone https://git.cooluc.com/sbwml/shortcut-fe package/new/shortcut-fe
 
 # FullCone module
-git clone https://git.cooluc.com/sbwml/nft-fullcone package/new/nft-fullcone
+# git clone https://git.cooluc.com/sbwml/nft-fullcone package/new/nft-fullcone
 
 # IPv6 NAT
 git clone https://github.com/sbwml/packages_new_nat6 package/new/nat6
