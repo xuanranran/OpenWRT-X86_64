0c9b2bc7dae9145e60356781cd8466b308b54b58
LovinYarn <juanshengyuan@gmail.com>
2024-11-18 15:27:05 +0800

    Update immortalwrt.sh

--- scripts/immortalwrt_24.10/immortalwrt.sh
+++ scripts/immortalwrt_24.10/immortalwrt.sh
@@ -15,10 +15,6 @@ pushd package/community
 git clone --depth=1 https://github.com/xuanranran/openwrt-package openwrt-package
 git clone --depth=1 https://github.com/xuanranran/rely openwrt-rely
 # git clone --depth=1 https://github.com/immortalwrt/wwan-packages wwan-packages
-# rm -rf openwrt-package/luci-theme-argon/htdocs/luci-static/argon/img/bg1.jpg
-# rm -rf openwrt-package/luci-theme-design/htdocs/luci-static/design/favicon.ico
-# cp -f $GITHUB_WORKSPACE/data/bg1.jpg openwrt-package/luci-theme-argon/htdocs/luci-static/argon/img/bg1.jpg
-# cp -f $GITHUB_WORKSPACE/data/favicon.ico openwrt-package/luci-theme-design/htdocs/luci-static/design/favicon.ico
 chmod 755 openwrt-package/luci-app-onliner/root/usr/share/onliner/setnlbw.sh
 popd
 
