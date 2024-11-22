e98895f313bbcb1c23ca72079324685c8dcd802c
LovinYarn <juanshengyuan@gmail.com>
2024-11-22 13:13:03 +0800

    Lean's Lede test 6.12

--- scripts/lean23.05/lean.sh
+++ scripts/lean23.05/lean.sh
@@ -57,9 +57,10 @@ sed -i '/mirror2.openwrt.org/a\push @mirrors, '\''https://source.cooluc.com'\'';
 
 # sed -i 's/+firewall/+uci-firewall/g' customfeeds/luci/applications/luci-app-firewall/Makefile
 
-# Test kernel 6.6
 rm -rf target/linux/x86/base-files/etc/board.d/02_network
 rm -rf package/base-files/files/etc/banner
 cp -f $GITHUB_WORKSPACE/data/banner package/base-files/files/etc/banner
 cp -f $GITHUB_WORKSPACE/data/02_network target/linux/x86/base-files/etc/board.d/02_network
-# sed -i 's/6.1/6.6/g' target/linux/x86/Makefile
+
+# Test kernel 6.12
+sed -i 's/6.6/6.12/g' target/linux/x86/Makefile
