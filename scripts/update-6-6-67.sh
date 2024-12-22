c37250b1e66ee28250bfd3f71dc20157cd572996
LovinYarn <juanshengyuan@gmail.com>
2024-12-22 13:40:40 +0800

    Revert "1"
    This reverts commit b9b7a9b3a2203ac27d2969846fb9257164eb24f9.

--- scripts/update-6-6-67.sh
+++
@@ -1,7 +0,0 @@
-#!/bin/bash
-# Set to local prepare
-
-curl -s https://raw.githubusercontent.com/openwrt/openwrt/refs/heads/main/include/kernel-6.6 > include/kernel-6.6
-curl -s https://raw.githubusercontent.com/openwrt/openwrt/refs/heads/main/target/linux/bcm27xx/patches-6.6/950-0106-Add-support-for-all-the-downstream-rpi-sound-card-dr.patch > target/linux/bcm27xx/patches-6.6/950-0106-Add-support-for-all-the-downstream-rpi-sound-card-dr.patch
-curl -s https://raw.githubusercontent.com/openwrt/openwrt/refs/heads/main/target/linux/generic/pending-6.6/100-compiler.h-only-include-asm-rwonce.h-for-kernel-code.patch > target/linux/generic/pending-6.6/100-compiler.h-only-include-asm-rwonce.h-for-kernel-code.patch
-curl -s https://raw.githubusercontent.com/openwrt/openwrt/refs/heads/main/target/linux/generic/pending-6.6/701-netfilter-nf_tables-ignore-EOPNOTSUPP-on-flowtable-d.patch > target/linux/generic/pending-6.6/701-netfilter-nf_tables-ignore-EOPNOTSUPP-on-flowtable-d.patch
