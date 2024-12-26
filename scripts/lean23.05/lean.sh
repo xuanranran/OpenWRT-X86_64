b56639054be9bd9aaabe3ff6de0100a114c1e2e6
LovinYarn <juanshengyuan@gmail.com>
2024-12-26 19:56:02 +0800

    Update lean.sh

--- scripts/lean23.05/lean.sh
+++ scripts/lean23.05/lean.sh
@@ -32,7 +32,7 @@ sed -i '$i uci set nlbwmon.@nlbwmon[0].refresh_interval=1s' zzz-default-settings
 sed -i '$i uci commit nlbwmon' zzz-default-settings
 sed -i '/http/d' zzz-default-settings
 sed -i '/23.05/d' zzz-default-settings
-export orig_version=$(cat "zzz-default-settings" | grep DISTRIB_REVISION= | awk -F "'" '{print $2}')
+export orig_version=$(cat "zzz-default-settings" | grep DISTRIB_DESCRIPTION= | awk -F "'" '{print $2}')
 export date_version=$(date -d "$(rdate -n -4 -p ntp.aliyun.com)" +'%Y-%m-%d')
 sed -i "s/${orig_version}/${orig_version} (${date_version})/g" zzz-default-settings
 popd
