6f8bb2bb99386faa04c89d7d3edcd78cf8fdf1ac
LovinYarn <juanshengyuan@gmail.com>
2025-01-07 14:23:13 +0800

    chore: refine

--- scripts/immortalwrt_24.10/immortalwrt.sh
+++ scripts/immortalwrt_24.10/immortalwrt.sh
@@ -18,14 +18,14 @@ git clone --depth=1 https://github.com/immortalwrt/wwan-packages wwan-packages
 chmod 755 openwrt-package/luci-app-onliner/root/usr/share/onliner/setnlbw.sh
 popd
 
-# Mod zzz-default-settings
-# pushd package/emortal/default-settings/default
-# sed -i '$i uci set nlbwmon.@nlbwmon[0].refresh_interval=1s' zzz-default-settings
-# sed -i '$i uci commit nlbwmon' zzz-default-settings
-# export orig_version=$(cat "zzz-default-settings" | grep DISTRIB_REVISION= | awk -F "'" '{print $2}')
-# export date_version=$(date -d "$(rdate -n -4 -p ntp.aliyun.com)" +'%Y-%m-%d')
-# sed -i "s/${orig_version}/${orig_version} (${date_version})/g" zzz-default-settings
-# popd
+Mod zzz-default-settings
+pushd package/emortal/default-settings/default
+sed -i '$i uci set nlbwmon.@nlbwmon[0].refresh_interval=1s' zzz-default-settings
+sed -i '$i uci commit nlbwmon' zzz-default-settings
+export orig_version=$(cat "zzz-default-settings" | grep DISTRIB_REVISION= | awk -F "'" '{print $2}')
+export date_version=$(date -d "$(rdate -n -4 -p ntp.aliyun.com)" +'%Y-%m-%d')
+sed -i "s/${orig_version}/${orig_version} (${date_version})/g" zzz-default-settings
+popd
 
 # fix sysupgrade
 rm -rf package/base-files/files/sbin/sysupgrade
