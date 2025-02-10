997fa98cbc25e45cf5fe0b53e2768a55624721a7
LovinYarn <juanshengyuan@gmail.com>
2025-02-10 20:40:37 +0800

    Update preset-mihomo-core.sh

--- scripts/preset-mihomo-core.sh
+++ scripts/preset-mihomo-core.sh
@@ -7,14 +7,14 @@
 # Author: LovinYarn
 # github: https://github.com/xuanranran
 #=================================================
-mkdir -p files/etc/mihomo/run/ui
+mkdir -p files/etc/nikki/run/ui
 
 GEOIP_URL="https://github.com/MetaCubeX/meta-rules-dat/releases/download/latest/geoip.dat"
 GEOSITE_URL="https://github.com/MetaCubeX/meta-rules-dat/releases/download/latest/geosite.dat"
 GEOIP_METADB_URL="https://github.com/MetaCubeX/meta-rules-dat/releases/download/latest/geoip-lite.metadb"
 ASN_MMDB_URL="https://github.com/MetaCubeX/meta-rules-dat/releases/download/latest/GeoLite2-ASN.mmdb"
 
-wget -qO- $GEOIP_URL > files/etc/mihomo/run/GeoIP.dat
-wget -qO- $GEOSITE_URL > files/etc/mihomo/run/GeoSite.dat
-wget -qO- $GEOIP_METADB_URL > files/etc/mihomo/run/geoip.metadb
-wget -qO- $ASN_MMDB_URL > files/etc/mihomo/run/ASN.mmdb
+wget -qO- $GEOIP_URL > files/etc/nikki/run/GeoIP.dat
+wget -qO- $GEOSITE_URL > files/etc/nikki/run/GeoSite.dat
+wget -qO- $GEOIP_METADB_URL > files/etc/nikki/run/geoip.metadb
+wget -qO- $ASN_MMDB_URL > files/etc/nikki/run/ASN.mmdb
