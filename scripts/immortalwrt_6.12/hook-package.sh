8e23a943322346bcaf413f503aa13d7c83cdcbec
LovinYarn <juanshengyuan@gmail.com>
2025-03-26 20:09:43 +0800

    Update hook-package.sh

--- scripts/immortalwrt_6.12/hook-package.sh
+++ scripts/immortalwrt_6.12/hook-package.sh
@@ -53,6 +53,19 @@ rm -rf customfeeds/packages/net/samba4
 git clone https://github.com/sbwml/feeds_packages_net_samba4 customfeeds/packages/net/samba4
 sed -i 's/ftp.gwdg.de/download.samba.org/g' customfeeds/packages/net/samba4/Makefile
 
+# frpc
+sed -i 's/procd_set_param stdout $stdout/procd_set_param stdout 0/g' customfeeds/packages/net/frp/files/frpc.init
+sed -i 's/procd_set_param stderr $stderr/procd_set_param stderr 0/g' customfeeds/packages/net/frp/files/frpc.init
+sed -i 's/stdout stderr //g' customfeeds/packages/net/frp/files/frpc.init
+sed -i '/stdout:bool/d;/stderr:bool/d' customfeeds/packages/net/frp/files/frpc.init
+sed -i '/stdout/d;/stderr/d' customfeeds/packages/net/frp/files/frpc.config
+sed -i 's/env conf_inc/env conf_inc enable/g' customfeeds/packages/net/frp/files/frpc.init
+sed -i "s/'conf_inc:list(string)'/& \\\\/" customfeeds/packages/net/frp/files/frpc.init
+sed -i "/conf_inc:list/a\\\t\t\'enable:bool:0\'" customfeeds/packages/net/frp/files/frpc.init
+sed -i '/procd_open_instance/i\\t\[ "$enable" -ne 1 \] \&\& return 1\n' customfeeds/packages/net/frp/files/frpc.init
+curl -s https://raw.githubusercontent.com/sbwml/r4s_build_script/refs/heads/master/openwrt/patch/luci/applications/luci-app-frpc/001-luci-app-frpc-hide-token.patch | patch -p1
+curl -s https://raw.githubusercontent.com/sbwml/r4s_build_script/refs/heads/master/openwrt/patch/luci/applications/luci-app-frpc/002-luci-app-frpc-add-enable-flag.patch | patch -p1
+
 # liburing - 2.7 (samba-4.21.0)
 rm -rf customfeeds/packages/libs/liburing
 git clone --depth 1 https://github.com/sbwml/feeds_packages_libs_liburing customfeeds/packages/libs/liburing
