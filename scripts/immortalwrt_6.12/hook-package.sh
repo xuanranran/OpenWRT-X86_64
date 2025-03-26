3899e4bae1882efb5060c4b98c78a5d6d958d2a4
LovinYarn <juanshengyuan@gmail.com>
2025-03-26 19:55:46 +0800

    fix

--- scripts/immortalwrt_6.12/hook-package.sh
+++ scripts/immortalwrt_6.12/hook-package.sh
@@ -21,11 +21,16 @@ sed -i 's/video,+libmesa +libwayland +libgudev/video,+libgudev/g' customfeeds/pa
 sed -i 's/controller,,+libgraphene +libjpeg +libpng/controller,,+libjpeg +libpng/g' customfeeds/packages/multimedia/gst1-plugins-base/Makefile
 
 # zerotier
-rm -rf customfeeds/packages/net/zerotier
-git clone https://github.com/sbwml/feeds_packages_net_zerotier customfeeds/packages/net/zerotier
+# rm -rf customfeeds/packages/net/zerotier
+# git clone https://github.com/sbwml/feeds_packages_net_zerotier customfeeds/packages/net/zerotier
 
-# sed -i 's/1.14.1/1.14.2/g' customfeeds/packages/net/zerotier/Makefile
-# sed -i 's/4f9f40b27c5a78389ed3f3216c850921f6298749e5819e9f2edabb2672ce9ca0/c2f64339fccf5148a7af089b896678d655fbfccac52ddce7714314a59d7bddbb/g' customfeeds/packages/net/zerotier/Makefile
+# mt76
+mkdir -p package/kernel/mt76/patches
+curl -s https://raw.githubusercontent.com/sbwml/r4s_build_script/refs/heads/master/openwrt/patch/mt76/patches/100-fix-build-with-mac80211-6.11-backport.patch > package/kernel/mt76/patches/100-fix-build-with-mac80211-6.11-backport.patch
+curl -s https://raw.githubusercontent.com/sbwml/r4s_build_script/refs/heads/master/openwrt/patch/mt76/patches/101-fix-build-with-linux-6.12rc2.patch > package/kernel/mt76/patches/101-fix-build-with-linux-6.12rc2.patch
+
+sed -i 's/1.14.1/1.14.2/g' customfeeds/packages/net/zerotier/Makefile
+sed -i 's/4f9f40b27c5a78389ed3f3216c850921f6298749e5819e9f2edabb2672ce9ca0/c2f64339fccf5148a7af089b896678d655fbfccac52ddce7714314a59d7bddbb/g' customfeeds/packages/net/zerotier/Makefile
 
 # Update golang 1.23.x
 rm -rf customfeeds/packages/lang/golang
