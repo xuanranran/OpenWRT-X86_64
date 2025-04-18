396d0e7c49b9b83e0158339e15d9273d51ee66c6
LovinYarn <juanshengyuan@gmail.com>
2025-04-18 18:48:37 +0800

    Update hook-package00.sh

--- scripts/immortalwrt_6.12/hook-package00.sh
+++ scripts/immortalwrt_6.12/hook-package00.sh
@@ -18,14 +18,14 @@ rm -rf customfeeds/packages/net/{*alist,chinadns-ng,dns2socks,dns2tcp,lucky,sing
 # chmod 755 customfeeds/lovepackages/luci-app-onliner/root/usr/share/onliner/setnlbw.sh
 
 # zerotier
-rm -rf customfeeds/packages/net/zerotier
-git clone https://github.com/xuanranran/feeds_packages_net_zerotier customfeeds/packages/net/zerotier
+# rm -rf customfeeds/packages/net/zerotier
+# git clone https://github.com/xuanranran/feeds_packages_net_zerotier customfeeds/packages/net/zerotier
 
-rm -rf customfeeds/luci/applications/luci-app-zerotier
-git clone https://github.com/xuanranran/luci-app-zerotier customfeeds/luci/applications/luci-app-zerotier
+# rm -rf customfeeds/luci/applications/luci-app-zerotier
+# git clone https://github.com/xuanranran/luci-app-zerotier customfeeds/luci/applications/luci-app-zerotier
 
-# sed -i 's/1.14.1/1.14.2/g' customfeeds/packages/net/zerotier/Makefile
-# sed -i 's/4f9f40b27c5a78389ed3f3216c850921f6298749e5819e9f2edabb2672ce9ca0/c2f64339fccf5148a7af089b896678d655fbfccac52ddce7714314a59d7bddbb/g' customfeeds/packages/net/zerotier/Makefile
+sed -i 's/1.14.1/1.14.2/g' customfeeds/packages/net/zerotier/Makefile
+sed -i 's/4f9f40b27c5a78389ed3f3216c850921f6298749e5819e9f2edabb2672ce9ca0/c2f64339fccf5148a7af089b896678d655fbfccac52ddce7714314a59d7bddbb/g' customfeeds/packages/net/zerotier/Makefile
 
 # mt76
 mkdir -p package/kernel/mt76/patches
