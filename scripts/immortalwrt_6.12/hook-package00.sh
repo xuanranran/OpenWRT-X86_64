4a1ec59d037851703ad906a832301580e3671f6b
LovinYarn <juanshengyuan@gmail.com>
2025-05-06 20:30:52 +0800

    1

--- scripts/immortalwrt_6.12/hook-package00.sh
+++ scripts/immortalwrt_6.12/hook-package00.sh
@@ -27,20 +27,6 @@ rm -rf customfeeds/packages/net/{*alist,chinadns-ng,dns2socks,dns2tcp,lucky,sing
 # sed -i 's/1.14.1/1.14.2/g' customfeeds/packages/net/zerotier/Makefile
 # sed -i 's/4f9f40b27c5a78389ed3f3216c850921f6298749e5819e9f2edabb2672ce9ca0/c2f64339fccf5148a7af089b896678d655fbfccac52ddce7714314a59d7bddbb/g' customfeeds/packages/net/zerotier/Makefile
 
-# mt76
-mkdir -p package/kernel/mt76/patches
-curl -s https://raw.githubusercontent.com/sbwml/r4s_build_script/refs/heads/master/openwrt/patch/mt76/patches/100-fix-build-with-mac80211-6.11-backport.patch > package/kernel/mt76/patches/100-fix-build-with-mac80211-6.11-backport.patch
-curl -s https://raw.githubusercontent.com/sbwml/r4s_build_script/refs/heads/master/openwrt/patch/mt76/patches/101-fix-build-with-linux-6.12rc2.patch > package/kernel/mt76/patches/101-fix-build-with-linux-6.12rc2.patch
-curl -s https://raw.githubusercontent.com/sbwml/r4s_build_script/refs/heads/master/openwrt/patch/mt76/patches/102-fix-build-with-mac80211-6.14-backport.patch > package/kernel/mt76/patches/102-fix-build-with-mac80211-6.14-backport.patch
-
-# mac80211 - 6.14
-rm -rf package/kernel/mac80211
-git clone https://github.com/sbwml/package_kernel_mac80211 package/kernel/mac80211 -b openwrt-24.10
-
-# ath10k-ct
-rm -rf package/kernel/ath10k-ct
-git clone https://github.com/sbwml/package_kernel_ath10k-ct package/kernel/ath10k-ct -b v6.14
-
 # bpf-headers - 6.12
 sed -ri "s/(PKG_PATCHVER:=)[^\"]*/\16.12/" package/kernel/bpf-headers/Makefile
 
@@ -120,9 +106,6 @@ curl -s https://raw.githubusercontent.com/xuanranran/r4s_build_script/refs/heads
 rm -rf customfeeds/packages/net/coova-chilli
 git clone https://github.com/sbwml/kmod_packages_net_coova-chilli customfeeds/packages/net/coova-chilli
 
-# tcp-brutal
-git clone https://github.com/sbwml/package_kernel_tcp-brutal package/kernel/tcp-brutal
-
 # vlmcsd
 # pushd customfeeds/packages/net
 # rm -rf vlmcsd
@@ -134,92 +117,3 @@ rm -rf customfeeds/luci/applications/luci-app-package-manager
 pushd customfeeds/luci/applications/
 git clone --depth 1 https://github.com/openwrt/luci openwrt_package-manager && mv -n openwrt_package-manager/applications/luci-app-package-manager ./ ; rm -rf openwrt_package-manager
 popd
-
-# gcc 15
-# gcc15 patches
-curl -s https://raw.githubusercontent.com/sbwml/r4s_build_script/refs/heads/master/openwrt/patch/generic-24.10/202-toolchain-gcc-add-support-for-GCC-15.patch | patch -p1
-# fix gcc-15
-sed -i '/TARGET_CFLAGS/ s/$/ -Wno-error=unterminated-string-initialization/' package/libs/mbedtls/Makefile
-# elfutils
-curl -s https://raw.githubusercontent.com/sbwml/r4s_build_script/refs/heads/master/openwrt/patch/openwrt-6.x/gcc-15/elfutils/901-backends-fix-string-initialization-error-on-gcc15.patch > package/libs/elfutils/patches/901-backends-fix-string-initialization-error-on-gcc15.patch
-# libwebsockets
-mkdir -p customfeeds/packages/libs/libwebsockets/patches
-curl -s https://raw.githubusercontent.com/sbwml/r4s_build_script/refs/heads/master/openwrt/patch/openwrt-6.x/gcc-15/libwebsockets/901-fix-string-initialization-error-on-gcc15.patch > customfeeds/packages/libs/libwebsockets/patches/901-fix-string-initialization-error-on-gcc15.patch
-# libxcrypt
-mkdir -p customfeeds/packages/libs/libxcrypt/patches
-curl -s https://raw.githubusercontent.com/sbwml/r4s_build_script/refs/heads/master/openwrt/patch/openwrt-6.x/gcc-15/libxcrypt/901-fix-string-initialization-error-on-gcc15.patch > customfeeds/packages/libs/libxcrypt/patches/901-fix-string-initialization-error-on-gcc15.patch
-
-# fix gcc-15.0.1 C23
-# gmp
-mkdir -p package/libs/gmp/patches
-curl -s https://raw.githubusercontent.com/sbwml/r4s_build_script/refs/heads/master/openwrt/patch/openwrt-6.x/gcc-15-c23/gmp/001-fix-build-with-gcc-15.patch > package/libs/gmp/patches/001-fix-build-with-gcc-15.patch
-# htop
-mkdir -p customfeeds/packages/admin/htop/patches
-curl -s https://raw.githubusercontent.com/sbwml/r4s_build_script/refs/heads/master/openwrt/patch/openwrt-6.x/gcc-15-c23/htop/001-Avoid-compilation-issues-with-ncurses-on-GCC-15.patch > customfeeds/packages/admin/htop/patches/001-Avoid-compilation-issues-with-ncurses-on-GCC-15.patch
-# libtirpc
-sed -i '/TARGET_CFLAGS/i TARGET_CFLAGS += -std=gnu17\n' customfeeds/packages/libs/libtirpc/Makefile
-# libsepol
-sed -i '/HOST_MAKE_FLAGS/i TARGET_CFLAGS += -std=gnu17\n' package/libs/libsepol/Makefile
-# tree
-sed -i '/MAKE_FLAGS/i TARGET_CFLAGS += -std=gnu17\n' customfeeds/packages/utils/tree/Makefile
-# gdbm
-sed -i '/CONFIGURE_ARGS/i TARGET_CFLAGS += -std=gnu17\n' customfeeds/packages/libs/gdbm/Makefile
-# libical
-sed -i '/CMAKE_OPTIONS/i TARGET_CFLAGS += -std=gnu17\n' customfeeds/packages/libs/libical/Makefile
-# libconfig
-sed -i '/CONFIGURE_ARGS/i TARGET_CFLAGS += -std=gnu17\n' package/customfeeds/packages/libconfig/Makefile
-# lsof
-sed -i '/CONFIGURE_ARGS/i TARGET_CFLAGS += -std=gnu17\n' customfeeds/packages/utils/lsof/Makefile
-# screen
-sed -i '/CONFIGURE_ARGS/i TARGET_CFLAGS += -std=gnu17\n' customfeeds/packages/utils/screen/Makefile
-# ppp
-sed -i '/CONFIGURE_VARS/i \\nTARGET_CFLAGS += -std=gnu17\n' package/network/services/ppp/Makefile
-# vim
-sed -i '/CONFIGURE_ARGS/i TARGET_CFLAGS += -std=gnu17\n' customfeeds/packages/utils/vim/Makefile
-# mtd
-sed -i '/target=/i TARGET_CFLAGS += -std=gnu17\n' package/system/mtd/Makefile
-# libselinux
-sed -i '/MAKE_FLAGS/i TARGET_CFLAGS += -std=gnu17\n' package/libs/libselinux/Makefile
-# avahi
-sed -i '/TARGET_CFLAGS +=/i TARGET_CFLAGS += -std=gnu17\n' customfeeds/packages/libs/avahi/Makefile
-# bash
-sed -i '/CONFIGURE_ARGS/i TARGET_CFLAGS += -std=gnu17\n' customfeeds/packages/utils/bash/Makefile
-# xl2tpd
-sed -i '/ifneq (0,0)/i TARGET_CFLAGS += -std=gnu17\n' customfeeds/packages/net/xl2tpd/Makefile
-# dnsmasq
-sed -i '/MAKE_FLAGS/i TARGET_CFLAGS += -std=gnu17\n' package/network/services/dnsmasq/Makefile
-# bluez
-sed -i '/CONFIGURE_ARGS/i TARGET_CFLAGS += -std=gnu17\n' customfeeds/packages/utils/bluez/Makefile
-# e2fsprogs
-sed -i '/CONFIGURE_ARGS/i TARGET_CFLAGS += -std=gnu17\n' package/utils/e2fsprogs/Makefile
-# f2fs-tools
-sed -i '/CONFIGURE_ARGS/i TARGET_CFLAGS += -std=gnu17\n' package/utils/f2fs-tools/Makefile
-# krb5
-sed -i '/CONFIGURE_VARS/i TARGET_CFLAGS += -std=gnu17\n' customfeeds/packages/net/krb5/Makefile
-# parted
-sed -i '/CONFIGURE_ARGS/i TARGET_CFLAGS += -std=gnu17\n' customfeeds/packages/utils/parted/Makefile
-# iperf3
-sed -i '/CONFIGURE_ARGS/i TARGET_CFLAGS += -std=gnu17\n' customfeeds/packages/net/iperf3/Makefile
-# db
-sed -i '/CONFIGURE_ARGS/i TARGET_CFLAGS += -std=gnu17\n' customfeeds/packages/libs/db/Makefile
-# python3
-sed -i '/TARGET_CONFIGURE_OPTS/i TARGET_CFLAGS += -std=gnu17\n' customfeeds/packages/lang/python/python3/Makefile
-# uwsgi
-sed -i '/MAKE_VARS/i TARGET_CFLAGS += -std=gnu17\n' customfeeds/packages/net/uwsgi/Makefile
-# perl
-sed -i '/Target perl/i TARGET_CFLAGS += -std=gnu17\n' customfeeds/packages/lang/perl/Makefile
-# rsync
-sed -i '/CONFIGURE_ARGS/i TARGET_CFLAGS += -std=gnu17\n' customfeeds/packages/net/rsync/Makefile
-# shine
-sed -i '/Build\/InstallDev/i TARGET_CFLAGS += -std=gnu17\n' customfeeds/packages/sound/shine/Makefile
-# jq
-sed -i '/CONFIGURE_ARGS/i TARGET_CFLAGS += -std=gnu17\n' customfeeds/packages/utils/jq/Makefile
-# coova-chilli
-sed -i '/TARGET_CFLAGS/s/$/ -std=gnu17/' customfeeds/packages/net/coova-chilli/Makefile
-
-# unzip
-rm -rf customfeeds/packages/utils/unzip
-git clone https://github.com/sbwml/feeds_packages_utils_unzip customfeeds/packages/utils/unzip
-
-# tcp-brutal
-git clone https://github.com/sbwml/package_kernel_tcp-brutal package/kernel/tcp-brutal
