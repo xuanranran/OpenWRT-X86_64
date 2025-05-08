ed416961b0268b0e8cbf856dd1bf9d2c96ae6b35
xuanranran <juanshengyuan@gmail.com>
2025-05-08 14:43:57 +0800

    Update hook-base.sh

--- scripts/immortalwrt/hook-base.sh
+++ scripts/immortalwrt/hook-base.sh
@@ -4,6 +4,7 @@
 # apk-tools
 curl -s https://raw.githubusercontent.com/xuanranran/r4s_build_script/refs/heads/master/openwrt/patch/apk-tools/9999-hack-for-linux-pre-releases.patch > package/system/apk/patches/9999-hack-for-linux-pre-releases.patch
 
+curl -s https://raw.githubusercontent.com/xuanranran/r4s_build_script/refs/heads/master/openwrt/patch/generic-24.10/0006-build-kernel-add-out-of-tree-kernel-config.patch | patch -p1
 curl -s https://raw.githubusercontent.com/xuanranran/r4s_build_script/refs/heads/master/openwrt/patch/generic-24.10/0005-kernel-Add-support-for-llvm-clang-compiler.patch | patch -p1
 
 # x86 - disable mitigations
