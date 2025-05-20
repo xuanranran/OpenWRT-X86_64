606ff797de8e0d8456625b5808d39c093326daa1
xuanranran <juanshengyuan@gmail.com>
2025-05-20 22:37:41 +0800

    Delete lean

--- scripts/immortalwrt/immortalwrt.sh
+++ scripts/immortalwrt/immortalwrt.sh
@@ -29,20 +29,6 @@ rm -rf dist-cdn-fonts.zip
 # git clone --depth 1 -b gh-pages https://github.com/Zephyruso/zashboard zashboard
 popd
 
-# Mod zzz-default-settings
-# pushd package/emortal/default-settings/default
-# sed -i '$i uci set nlbwmon.@nlbwmon[0].refresh_interval=1s' zzz-default-settings
-# sed -i '$i uci commit nlbwmon' zzz-default-settings
-# export orig_version=$(cat "zzz-default-settings" | grep DISTRIB_REVISION= | awk -F "'" '{print $2}')
-# export date_version=$(date -d "$(rdate -n -4 -p ntp.aliyun.com)" +'%Y-%m-%d')
-# sed -i "s/${orig_version}/${orig_version} (${date_version})/g" zzz-default-settings
-# popd
-
-# fix sysupgrade
-# rm -rf package/base-files/files/sbin/sysupgrade
-# cp -f $GITHUB_WORKSPACE/data/sysupgrade package/base-files/files/sbin/sysupgrade
-# chmod 755 package/base-files/files/sbin/sysupgrade
-
 # Change default shell to zsh
 sed -i 's/\/bin\/ash/\/usr\/bin\/zsh/g' package/base-files/files/etc/passwd
 
@@ -55,9 +41,7 @@ sed -i "s/ImmortalWrt/OpenWrt/g" package/base-files/files/bin/config_generate
 sed -i 's/mirror.iscas.ac.cn/mirrors.ustc.edu.cn/g' scripts/projectsmirrors.json
 
 sed -i 's/services/network/g' customfeeds/luci/applications/luci-app-upnp/root/usr/share/luci/menu.d/luci-app-upnp.json
-
 sed -i 's/services/network/g' customfeeds/luci/applications/luci-app-frpc/root/usr/share/luci/menu.d/luci-app-frpc.json
-
 
 # other
 rm -rf target/linux/x86/base-files/etc/board.d/02_network
