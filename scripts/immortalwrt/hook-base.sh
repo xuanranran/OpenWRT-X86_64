6678454927ed696d4ae62f9985e9e65d77e113f9
xuanranran <juanshengyuan@gmail.com>
2025-05-29 20:03:48 +0800

    0

--- scripts/immortalwrt/hook-base.sh
+++ scripts/immortalwrt/hook-base.sh
@@ -98,8 +98,8 @@ sed -i 's/procd_set_param stdout 1/procd_set_param stdout 0/g' customfeeds/packa
 sed -i 's/procd_set_param stderr 1/procd_set_param stderr 0/g' customfeeds/packages/utils/ttyd/files/ttyd.init
 
 # UPnP
-rm -rf customfeeds/packages/net/miniupnpd
-git clone https://git.cooluc.com/sbwml/miniupnpd customfeeds/packages/net/miniupnpd -b v2.3.9
+# rm -rf customfeeds/packages/net/miniupnpd
+# git clone https://git.cooluc.com/sbwml/miniupnpd customfeeds/packages/net/miniupnpd -b v2.3.9
 # git clone https://git.cooluc.com/sbwml/luci-app-upnp customfeeds/luci/applications/luci-app-upnp -b openwrt-24.10
 
 # Nginx
