4d5fd9f43a33723511a78af332feee657acdcd7d
xuanranran <juanshengyuan@gmail.com>
2025-06-17 01:04:47 +0800

    Update preset-nikki-core.sh

--- scripts/preset-nikki-core.sh
+++ scripts/preset-nikki-core.sh
@@ -19,6 +19,16 @@ wget -qO- $GEOSITE_URL > files/etc/nikki/run/GeoSite.dat
 wget -qO- $GEOIP_METADB_URL > files/etc/nikki/run/geoip.metadb
 wget -qO- $ASN_MMDB_URL > files/etc/nikki/run/ASN.mmdb
 
+pushd files/etc/nikki/run/ui/
+curl -sSL https://codeload.github.com/haishanh/yacd/zip/refs/heads/gh-pages -o yacd-dist-cdn-fonts.zip
+curl -sSL https://github.com/MetaCubeX/metacubexd/releases/latest/download/compressed-dist.tgz -o compressed-dist.tgz
+curl -sSL https://github.com/Zephyruso/zashboard/releases/latest/download/dist-cdn-fonts.zip -o dist-cdn-fonts.zip
+tar zxf compressed-dist.tgz -C ./metacubexd
+unzip -q dist-cdn-fonts.zip && unzip -q yacd-dist-cdn-fonts.zip
+mv dist zashboard && mv yacd-gh-pages yacd
+rm -rf yacd-dist-cdn-fonts.zip dist-cdn-fonts.zip compressed-dist.tgz
+popd
+
 # mkdir -p files/etc/fchomo/
 # wget -qO- $GEOIP_URL > files/etc/fchomo/geoip.dat
 # wget -qO- $GEOSITE_URL > files/etc/fchomo/geosite.dat
