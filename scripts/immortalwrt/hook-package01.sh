23ec6c288a24e8cb3935a4bf1dc0463f484579f2
xuanranran <juanshengyuan@gmail.com>
2025-07-27 21:18:13 +0800

    1

--- scripts/immortalwrt/hook-package01.sh
+++ scripts/immortalwrt/hook-package01.sh
@@ -21,7 +21,7 @@ curl -s https://raw.githubusercontent.com/sbwml/r4s_build_script/refs/heads/mast
 curl -s https://raw.githubusercontent.com/sbwml/r4s_build_script/refs/heads/master/openwrt/patch/packages-patches/batman-adv/901-fix-linux-6.12rc2-builds.patch > customfeeds/routing/batman-adv/patches/901-fix-linux-6.12rc2-builds.patch
 
 # libsodium - fix build with lto (GNU BUG - 89147)
-sed -i "/CONFIGURE_ARGS/i\TARGET_CFLAGS += -ffat-lto-objects\n" customfeeds/packages/libs/libsodium/Makefile
+sed -i "/CONFIGURE_ARGS/i\TARGET_CFLAGS += -std=gnu17\n" customfeeds/packages/libs/libsodium/Makefile
 
 # sms-tools
 mkdir -p customfeeds/packages/utils/sms-tool/patches
