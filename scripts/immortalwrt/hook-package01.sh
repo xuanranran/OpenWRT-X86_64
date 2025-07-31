264fee32df952a4abf1de028d95ccee49253a234
Lovin Yarn <juanshengyuan@gmail.com>
2025-07-31 14:26:12 +0800

    Add bison Makefile fetch to hook script
    The script now downloads the bison Makefile from the OpenWrt repository and saves it to tools/bison/Makefile. This addition automates the retrieval of the latest bison build instructions.

--- scripts/immortalwrt/hook-package01.sh
+++ scripts/immortalwrt/hook-package01.sh
@@ -30,3 +30,6 @@ curl -s https://raw.githubusercontent.com/sbwml/r4s_build_script/refs/heads/mast
 # bind
 rm -rf customfeeds/packages/net/bind
 git clone --depth=1 https://github.com/xuanranran/feeds_packages_net_bind customfeeds/packages/net/bind
+
+# bison
+curl -s https://raw.githubusercontent.com/openwrt/openwrt/refs/heads/main/tools/bison/Makefile > tools/bison/Makefile
