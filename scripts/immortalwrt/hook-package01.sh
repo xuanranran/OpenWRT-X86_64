2fdc19ca8b01ee5e58db9bc49fa60cb6b3fdefda
Lovin Yarn <juanshengyuan@gmail.com>
2025-10-23 20:10:00 +0800

    Update hook-package01.sh

--- scripts/immortalwrt/hook-package01.sh
+++ scripts/immortalwrt/hook-package01.sh
@@ -2,14 +2,14 @@
 # Set to local prepare
 
 # libpfring
-rm -rf customfeeds/packages/libs/libpfring
-mkdir -p customfeeds/packages/libs/libpfring/patches
-curl -s https://raw.githubusercontent.com/sbwml/r4s_build_script/refs/heads/master/openwrt/patch/packages-patches/libpfring/Makefile > customfeeds/packages/libs/libpfring/Makefile
-pushd customfeeds/packages/libs/libpfring/patches
-  curl -Os https://raw.githubusercontent.com/sbwml/r4s_build_script/refs/heads/master/openwrt/patch/packages-patches/libpfring/patches/0001-fix-cross-compiling.patch
-  curl -Os https://raw.githubusercontent.com/sbwml/r4s_build_script/refs/heads/master/openwrt/patch/packages-patches/libpfring/patches/100-fix-compilation-warning.patch
-  curl -Os https://raw.githubusercontent.com/sbwml/r4s_build_script/refs/heads/master/openwrt/patch/packages-patches/libpfring/patches/900-fix-linux-6.6.patch
-popd
+# rm -rf customfeeds/packages/libs/libpfring
+# mkdir -p customfeeds/packages/libs/libpfring/patches
+# curl -s https://raw.githubusercontent.com/sbwml/r4s_build_script/refs/heads/master/openwrt/patch/packages-patches/libpfring/Makefile > customfeeds/packages/libs/libpfring/Makefile
+# pushd customfeeds/packages/libs/libpfring/patches
+  # curl -Os https://raw.githubusercontent.com/sbwml/r4s_build_script/refs/heads/master/openwrt/patch/packages-patches/libpfring/patches/0001-fix-cross-compiling.patch
+  # curl -Os https://raw.githubusercontent.com/sbwml/r4s_build_script/refs/heads/master/openwrt/patch/packages-patches/libpfring/patches/100-fix-compilation-warning.patch
+  # curl -Os https://raw.githubusercontent.com/sbwml/r4s_build_script/refs/heads/master/openwrt/patch/packages-patches/libpfring/patches/900-fix-linux-6.6.patch
+# popd
 
 # rtpengine
 curl -s https://raw.githubusercontent.com/sbwml/r4s_build_script/refs/heads/master/openwrt/patch/packages-patches/rtpengine/900-fix-linux-6.12-11.5.1.18.patch > customfeeds/telephony/net/rtpengine/patches/900-fix-linux-6.12-11.5.1.18.patch
