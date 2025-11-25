2b1006fb0d2fdc93cfbc4b76dee0f52b3625db3d
Lovin Yarn <juanshengyuan@gmail.com>
2025-11-25 20:25:46 +0800

    Update hook-package01.sh

--- scripts/immortalwrt/hook-package01.sh
+++ scripts/immortalwrt/hook-package01.sh
@@ -10,4 +10,4 @@ curl -s https://raw.githubusercontent.com/sbwml/r4s_build_script/refs/heads/mast
 # libxcrypt
 sed -i "/CONFIGURE_ARGS/i\TARGET_CFLAGS += -Wno-error=pedantic\n" package/libs/xcrypt/libxcrypt/Makefile
 
-rm -rf target/linux/generic/backport-6.12/404-v6.19-mtd-spinand-fmsh-remove-QE-bit-for-FM25S01A-flash.patch
+rm -rf target/linux/generic/hack-6.12/400-mtd-spinand-Support-fmsh.patch
