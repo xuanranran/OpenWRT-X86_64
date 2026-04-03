bef4d3d032b266f12832de3821f81dfb1ceb320b
Lovin Yarn <juanshengyuan@gmail.com>
2026-04-03 12:14:36 +0800

    6.18
    Update CI/workflow and build hooks for Linux 6.18 and Hyper-V support. Renamed workflow instance, added execution of a new scripts/check-hyperv-config.sh (verifies target fragment and final kernel .config) and made it executable. Introduced many package and kernel adjustments in hook-base.sh and hook-package.sh: backports/patches for BBRv3, LRNG, linux-rt, mac80211, kernel btf/cpu/net patches, DPDK/numactl support, firewall4/nftables/fullcone/shortcut-fe/natflow/luci patches, and other package fixes. Added new check-hyperv-config.sh script. Expanded data/02_network with numerous board-specific network device path mappings, virtual platform detection, and a default fallback. Tweaked configs/immortalwrt/immortalwrt.config to adjust target options (GRUB timeout, images, DPDK/natflow settings) and removed unused/multi-profile flags. Also consolidated/renamed package hook script (hook-package00.sh -> hook-package.sh) and removed obsolete package entries.

--- scripts/immortalwrt/hook-package.sh
+++ scripts/immortalwrt/hook-package.sh
@@ -60,3 +60,57 @@ git clone https://git.cooluc.com/sbwml/feeds_packages_libs_dmx_usb_module custom
 # coova-chilli module
 rm -rf customfeeds/packages/net/coova-chilli
 git clone https://$github/sbwml/kmod_packages_net_coova-chilli customfeeds/packages/net/coova-chilli
+
+# nat46
+mkdir -p package/kernel/nat46/patches
+curl -s $mirror/openwrt/patch/packages-patches/nat46/102-fix-build-with-kernel-6.18.patch > package/kernel/nat46/patches/102-fix-build-with-kernel-6.18.patch
+
+# openvswitch
+sed -i '/ovs_kmod_openvswitch_depends/a\\t\ \ +kmod-sched-act-sample \\' customfeeds/packages/net/openvswitch/Makefile
+
+# rtpengine
+curl -s $mirror/openwrt/patch/packages-patches/rtpengine/901-fix-build-for-linux-6.18.patch > customfeeds/telephony/net/rtpengine/patches/901-fix-build-for-linux-6.18.patch
+
+# usb-serial-xr_usb_serial_common: remove package
+# Now that we have packaged the upstream driver[1] and only board[2] that
+# includes it by default has been switched to it, remove this out-of-tree
+# driver that is broken on 6.12 anyway.
+rm -rf customfeeds/packages/libs/xr_usb_serial_common
+
+# v4l2loopback
+rm -rf customfeeds/packages/kernel/v4l2loopback
+mkdir -p customfeeds/packages/kernel/v4l2loopback
+curl -s $mirror/openwrt/patch/packages-patches/v4l2loopback/Makefile > customfeeds/packages/kernel/v4l2loopback/Makefile
+
+# telephony
+pushd customfeeds/telephony
+  # dahdi-linux
+  rm -rf libs/dahdi-linux
+  git clone https://$github/sbwml/feeds_telephony_libs_dahdi-linux libs/dahdi-linux -b v6.18
+popd
+
+# boots
+sed -i 's|^PKG_SOURCE_URL:=.*|PKG_SOURCE_URL:=@SF/$(PKG_NAME)/$(PKG_NAME)/$(PKG_VERSION)|g' customfeeds/packages/libs/boost/Makefile
+
+# fix gcc-15.0.1 gnu17
+
+# xl2tpd
+sed -i '/ifneq (0,0)/i TARGET_CFLAGS += -std=gnu17\n' customfeeds/packages/net/xl2tpd/Makefile
+# netdata
+sed -i '/TARGET_CFLAGS/i TARGET_CFLAGS += -std=gnu17\n' customfeeds/packages/admin/netdata/Makefile
+# uwsgi
+sed -i '/MAKE_VARS/i TARGET_CFLAGS += -std=gnu17\n' customfeeds/packages/net/uwsgi/Makefile
+# libpam
+sed -i '/MESON_ARGS/i TARGET_CFLAGS += -std=gnu17\n' customfeeds/packages/libs/libpam/Makefile
+# coova-chilli - fix gcc 15 c23
+sed -i '/TARGET_CFLAGS/s/$/ -std=gnu17/' customfeeds/packages/net/coova-chilli/Makefile
+
+sed -i '/^CONFIG_FAILOVER=y$/a # CONFIG_SHORTCUT_FE is not set' target/linux/x86/64/config-6.18
+
+# libnftnl
+rm -rf package/libs/libnftnl/patches
+# nftables
+rm -rf package/network/utils/nftables/patches
+
+# del packages
+rm -rf customfeeds/packages/net/onionshare-cli
