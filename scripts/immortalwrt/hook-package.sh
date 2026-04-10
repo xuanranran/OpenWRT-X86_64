b11cd7b8300fd53c4ae329b895df2e732b082998
Lovin Yarn <juanshengyuan@gmail.com>
2026-04-10 15:46:50 +0800

    Update for Linux 6.18
    Switch CI workflow to immortalwrt_master_6_18 and invoke the consolidated hook-package.sh; remove the old secondary package hook. Move many kernel/package patches and backports from 6.12 to 6.18 (BBRv3, LRNG, linux-rt, bpf-headers, mac80211 and other kernel fixes). Add/clone updated drivers and packages (Realtek r8168/r8125/r8126/r8127/r8101, rtl8822cs, rtl8852au, DPDK, numactl, natflow, shortcut-fe, nat6) and apply multiple firewall/luci/nft patches and fixes for 6.18. Expand board network mappings and add virtual platform detection with dynamic SFP handling in data/02_network. Update configs to enable dpdk-tools, numactl, natflow and luci-plugin-2fa and adjust GRUB/target settings. Tidy shell config (zshcompdump and compfix) and add rootfs sysctl defaults. Remove or delete obsolete/out-of-tree packages and clean up various package patches and build flags (GNU17 fixes for some packages, openvswitch tweak, miniupnpd source change).
    
    Signed-off-by: Lovin Yarn <juanshengyuan@gmail.com>
    
    Update .zshrc
    
    Update hook-base.sh
    
    Update immortalwrt.sh
    
    Update hook-base.sh
    
    Update immortalwrt.config
    
    Update immortalwrt_master.yml
    
    Update hook-package.sh
    
    Remove xtables-addons clone in hook script
    
    Delete the commands that removed and re-cloned customfeeds/packages/net/xtables-addons from scripts/immortalwrt/hook-package.sh. This avoids forcibly replacing the xtables-addons feed during the package hook while keeping the subsequent clang/netatop adjustments intact.

--- scripts/immortalwrt/hook-package.sh
+++ scripts/immortalwrt/hook-package.sh
@@ -24,6 +24,15 @@ rm -rf customfeeds/packages/net/{*alist,chinadns-ng,dns2socks,dns2tcp,lucky,sing
 rm -rf customfeeds/packages/lang/golang
 git clone https://github.com/sbwml/packages_lang_golang customfeeds/packages/lang/golang
 
+# Docker
+rm -rf customfeeds/luci/applications/luci-app-dockerman
+git clone https://github.com/sbwml/luci-app-dockerman -b openwrt-25.12 customfeeds/luci/applications/luci-app-dockerman
+rm -rf customfeeds/packages/utils/{docker,dockerd,containerd,runc}
+git clone https://github.com/sbwml/packages_utils_docker customfeeds/packages/utils/docker
+git clone https://github.com/sbwml/packages_utils_dockerd customfeeds/packages/utils/dockerd
+git clone https://github.com/sbwml/packages_utils_containerd customfeeds/packages/utils/containerd
+git clone https://github.com/sbwml/packages_utils_runc customfeeds/packages/utils/runc
+
 # samba4 - bump version
 # rm -rf customfeeds/packages/net/samba4
 # git clone https://github.com/sbwml/feeds_packages_net_samba4 customfeeds/packages/net/samba4
@@ -46,9 +55,6 @@ rm -rf package/network/utils/xdp-tools
 git clone --depth 1 https://github.com/sbwml/package_network_utils_xdp-tools package/network/utils/xdp-tools
 
 # clang
-# xtables-addons module
-rm -rf customfeeds/packages/net/xtables-addons
-git clone https://$github/sbwml/kmod_packages_net_xtables-addons customfeeds/packages/net/xtables-addons -b openwrt-25.12
 # netatop
 sed -i 's/$(MAKE)/$(KERNEL_MAKE)/g' customfeeds/packages/admin/netatop/Makefile
 curl -s $mirror/openwrt/patch/packages-patches/clang/netatop/900-fix-build-with-clang.patch > customfeeds/packages/admin/netatop/patches/900-fix-build-with-clang.patch
@@ -60,3 +66,57 @@ git clone https://git.cooluc.com/sbwml/feeds_packages_libs_dmx_usb_module custom
 # coova-chilli module
 rm -rf customfeeds/packages/net/coova-chilli
 git clone https://$github/sbwml/kmod_packages_net_coova-chilli customfeeds/packages/net/coova-chilli
+
+# nat46
+mkdir -p package/kernel/nat46/patches
+curl -s $mirror/openwrt/patch/packages-patches/nat46/102-fix-build-with-kernel-6.18.patch > package/kernel/nat46/patches/102-fix-build-with-kernel-6.18.patch
+
+# openvswitch
+sed -i '/ovs_kmod_openvswitch_depends/a\\t\ \ +kmod-sched-act-sample \\' customfeeds/packages/net/openvswitch/Makefile
+
+# rtpengine
+curl -s $mirror/openwrt/patch/packages-patches/rtpengine/901-fix-build-for-linux-6.18.patch > customfeeds/telephony/net/rtpengine/patches/901-fix-build-for-linux-6.18.patch
+
+# usb-serial-xr_usb_serial_common: remove package
+# Now that we have packaged the upstream driver[1] and only board[2] that
+# includes it by default has been switched to it, remove this out-of-tree
+# driver that is broken on 6.12 anyway.
+rm -rf customfeeds/packages/libs/xr_usb_serial_common
+
+# v4l2loopback
+rm -rf customfeeds/packages/kernel/v4l2loopback
+mkdir -p customfeeds/packages/kernel/v4l2loopback
+curl -s $mirror/openwrt/patch/packages-patches/v4l2loopback/Makefile > customfeeds/packages/kernel/v4l2loopback/Makefile
+
+# telephony
+pushd customfeeds/telephony
+  # dahdi-linux
+  rm -rf libs/dahdi-linux
+  git clone https://$github/sbwml/feeds_telephony_libs_dahdi-linux libs/dahdi-linux -b v6.18
+popd
+
+# boots
+sed -i 's|^PKG_SOURCE_URL:=.*|PKG_SOURCE_URL:=@SF/$(PKG_NAME)/$(PKG_NAME)/$(PKG_VERSION)|g' customfeeds/packages/libs/boost/Makefile
+
+# fix gcc-15.0.1 gnu17
+
+# xl2tpd
+sed -i '/ifneq (0,0)/i TARGET_CFLAGS += -std=gnu17\n' customfeeds/packages/net/xl2tpd/Makefile
+# netdata
+sed -i '/TARGET_CFLAGS/i TARGET_CFLAGS += -std=gnu17\n' customfeeds/packages/admin/netdata/Makefile
+# uwsgi
+sed -i '/MAKE_VARS/i TARGET_CFLAGS += -std=gnu17\n' customfeeds/packages/net/uwsgi/Makefile
+# libpam
+sed -i '/MESON_ARGS/i TARGET_CFLAGS += -std=gnu17\n' customfeeds/packages/libs/libpam/Makefile
+# coova-chilli - fix gcc 15 c23
+sed -i '/TARGET_CFLAGS/s/$/ -std=gnu17/' customfeeds/packages/net/coova-chilli/Makefile
+
+sed -i '/^CONFIG_FAILOVER=y$/a # CONFIG_SHORTCUT_FE is not set' target/linux/x86/64/config-6.18
+
+# libnftnl
+rm -rf package/libs/libnftnl/patches
+# nftables
+rm -rf package/network/utils/nftables/patches
+
+# del packages
+rm -rf customfeeds/packages/net/onionshare-cli
